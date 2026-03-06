270063104fa982c67ff86da849b48f7fa3e9feb3
Mariam Oduye <oduyemariam@gmail.com>
2026-03-06 12:20:13 +0100

    Update

--- script.js
+++ script.js
@@ -24,9 +24,11 @@
   }
 
   function peep(){
-    const time = randomTime(500,1000)
+    const time = randomTime(500,2000)
     const hole = randomHole(holes);
     hole.classList.add('up')
+    hole.classList.remove('hit');
+
     setTimeout(() => {
       hole.classList.remove('up');
       if(!timeUp) peep();
@@ -35,29 +37,36 @@
   }
 
   function startGame(){
-    scoreBoard.textContent = 0;
-    message.textContent = "";
-    timeUp = false;
-    score = 0;
-    peep()
+  scoreBoard.textContent = 0;
+  message.textContent = "";
+  timeUp = false;
+  score = 0;
+  peep();
 
-    setTimeout(() =>{
-      timeUp = true
-      message.textContent = `Game Over! Your score is ${score}. Click Start to play again.`;
+  // Stop game after 10 seconds
+  setTimeout(() => {
+    timeUp = true;  
+    if(score >= 5){
+      message.textContent = `😊Good Job! Your score is ${score}. Click Start to play again.`;
+    } else {
+      message.textContent = `😢Game Over! Your score is ${score}. Click Start to play again.`;
+    }
 
-      confetti({
+    confetti({
       particleCount: 150,
       spread: 100,
       origin: { y: 0.6 }
     });
-    }, 10000)
-  }
-
+  }, 10000);
+}
   function bonk(e){
     if(!e.isTrusted) return;
+    if(this.classList.contains('hit')) return; 
+
     score++;
     this.classList.remove('up');
+    this.classList.add('hit');
     scoreBoard.textContent = score
   }
 
-  moles.forEach(mole => mole.addEventListener('click', bonk))
+  moles.forEach(mole => mole.addEventListener('pointerup', bonk));
